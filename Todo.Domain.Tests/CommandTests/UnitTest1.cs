f637e7a1c88c65c2485f68483ed64e0c247237a2
Daniel Rodrigues <dantdmc210@gmail.com>
2020-08-31 13:10:21 -0300

    Refatorando testes

--- Todo.Domain.Tests/CommandTests/UnitTest1.cs
+++ Todo.Domain.Tests/CommandTests/UnitTest1.cs
@@ -7,20 +7,25 @@ namespace Todo.Domain.Tests.CommandTests
     [TestClass]
     public class UnitTest1
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.UtcNow);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da tarefa", "usuario", DateTime.UtcNow);
+
+        public UnitTest1()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido()
         {
-            var command = new CreateTodoCommand("", "", DateTime.UtcNow);
-            command.Validate();
-            Assert.AreEqual(command.Valid, false);
+            Assert.AreEqual(_invalidCommand.Valid, false);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido()
         {
-            var command = new CreateTodoCommand("Titulo da tarefa", "usuario", DateTime.UtcNow);
-            command.Validate();
-            Assert.AreEqual(command.Valid, true);
+            Assert.AreEqual(_validCommand.Valid, true);
         }
 
         
