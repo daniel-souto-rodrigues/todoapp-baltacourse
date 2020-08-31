f54d433f645eb22237c28416a26d9c7fafcfcbef
Daniel Rodrigues <dantdmc210@gmail.com>
2020-08-31 10:41:22 -0300

    Entidade TodoItem

---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public TodoItem(string title, bool done, DateTime date, string user)
+        {
+            this.Title = title;
+            this.Done = false;
+            this.Date = date;
+            this.User = user;
+        }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
